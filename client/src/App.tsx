a0f7c6655127822f7be2d33cc37989623de18fec
AlanVega <alanvega002@gmail.com>
2020-06-04 02:57:55 -0800

    input funcitonality + emojis

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,100 +1,41 @@
 import React, { useState, useEffect } from "react";
-// import TargetBox from "./components/Dnd";
-// import { DndProvider } from "react-dnd";
-// import { HTML5Backend } from "react-dnd-html5-backend";
-import ImageUploader from "react-images-upload";
+import { displayGif, audioUpload } from "./fileHandler/fileHandler";
+import ReactAudioPlayer from "react-audio-player";
+import Form from "react-bootstrap/Form";
+import "emoji-mart/css/emoji-mart.css";
+import { Picker } from "emoji-mart";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
+interface protectedElements {
+  readonly inputElement: HTMLInputElement;
+}
+
 function App() {
-  const [pictures, setPictures] = useState<File[]>([]);
-  const [gif, setGif] = useState<string>();
-  const onDrop = (picture: any) => {
-    setPictures([...pictures, picture]);
-    console.log(pictures);
-  };
-
-  // const fileToImage = () => {
-  //   const label;
-  // };
-  // console.log(pictures);
-  // const fileReader = (gif: any) => {
-  //   let file = gif;
-  //   let reader = new FileReader();
-
-  //   reader.readAsDataURL(file);
-
-  //   reader.onload = () => {
-  //     console.log(reader.result);
-  //   };s
-
-  //   reader.onerror = () => {
-  //     console.log(reader.error);
-  //   };
-  //   setPictures([...pictures, gif]);
-  // };
-
-  useEffect(() => {
-    if (pictures) {
-      let blob = new Blob(pictures, { type: "image/gif" });
-      let reader = new FileReader();
-
-      reader.readAsDataURL(blob);
-
-      reader.addEventListener(event) {
-        console.log(reader.result);
-        // let output = reader.result ? reader.result.toString() : "null";
-        let img = document.getElementById("gif") as HTMLImageElement;
-        if (!(img === null)) img.src = (URL || webkitURL).createObjectURL(blob);
-      };
-      // setGif(reader.readAsDataURL(blob));
-      // "data:image/png;base64,R0lGODlhDAAMAKIFAF5LAP/zxAAAANyuAP/gaP///wAAAAAAACH5BAEAAAUALAAAAAAMAAwAAAMlWLPcGjDKFYi9lxKBOaGcF35DhWHamZUW0K4mAbiwWtuf0uxFAgA7"
-      //   reader.onload = function () {
-      //     let dataURL = reader.result;
-      //     let output:HTMLImageElement = document.getElementById("gif")!;
-      //     output.src =
-      //   };
-    }
-  }, [pictures]);
-
-  // function displayGif(gif: any) {
-  //   console.log(gif);
-  //   console.log(typeof gif);
-  //   console.log(typeof gif[0]);
-  //   // const preview: HTMLImageElement | null = document.getImageElementById(
-  //   //   "gif"
-  //   // );
-
-  //   const reader = new FileReader();
-
-  //   reader.addEventListener(
-  //     "load",
-  //     function () {
-  //       // convert image file to base64 string
-  //       setGif(reader.result?.toString());
-  //     },
-  //     false
-  //   );
-
-  // if (gif) {
-  //   console.log("test");
-
-  //   reader.readAsDataURL(gif);
-  // }
-  // }
-
   return (
     <div className="App">
-      <ImageUploader
-        className={"gif"}
-        withIcon={false}
-        onChange={onDrop}
-        withLabel={false}
-        imgExtension={[".gif"]}
-        maxFileSize={5242880}
-        withPreview={false}
-        singleImage={true}
-      />
-      <img id="gif" alt="test" />
+      <Form>
+        <Form.File
+          id="gifInput"
+          label="Upload GIF..."
+          lang="en"
+          onChange={displayGif}
+          custom
+        />
+      </Form>
+      <Form>
+        <Form.File
+          id="audioInput"
+          label="Upload audio..."
+          lang="en"
+          onChange={audioUpload}
+          custom
+        />
+      </Form>
+      <img id="gif" src="" height="200" width="300" alt="Image preview..." />
+      <ReactAudioPlayer id="audio" src="" controls></ReactAudioPlayer>
+      <br />
+      <Picker set="apple" />
     </div>
   );
 }
